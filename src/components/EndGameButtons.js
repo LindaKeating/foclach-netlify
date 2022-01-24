f25001edd8a7711a2f6c61be06842aecef7c5aa7
Linda Keating <linda_keating@me.com>
2022-01-24 11:24:22 +0000

    remove share button from practice mode

--- src/components/EndGameButtons.js
+++ src/components/EndGameButtons.js
@@ -2,10 +2,6 @@ const EndGameButtons = (props ) => {
     const isVisible = props.isOpen
     if (isVisible) {
         return <div className="endGameButtons">
-        <button className="endGameButton shareButton" 
-            onClick={props.shareResults}>
-            ⭐   Roinn  ⭐ 
-        </button>
         <button className="endGameButton"
            onClick={props.playAgain}>
             😊 Imir Arís
